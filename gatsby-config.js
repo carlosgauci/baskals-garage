c3a838b036c6aa61c7095d4e34af5e3ec8039a35
Carlos <53526597+carlosgauci@users.noreply.github.com>
2021-03-08 20:22:40 +0100

    added and styled header

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,14 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-plugin-sass`,
     {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /images/,
+        },
+      },
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,18 @@
 import React from "react"
+import { header, logo, clippedBG, navIcon } from "./Header.module.scss"
+import Logo from "../../images/svg/logo.svg"
+import NavIcon from "../../images/svg/nav.svg"
 
-const Header = () => <header></header>
+const Header = () => (
+  <header className={header}>
+    <div className={clippedBG} />
+    <section className={logo}>
+      <Logo />
+    </section>
+    <div className={navIcon}>
+      <NavIcon />
+    </div>
+  </header>
+)
 
 export default Header
