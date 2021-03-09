d9c458f6e44293035eb2234107e6e8f2fbf6c513
Carlos <53526597+carlosgauci@users.noreply.github.com>
2021-03-09 17:53:36 +0100

    added Contact component

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,49 @@
+import React from "react"
+import { LocationPin, Mail, Phone, Clock, Facebook } from "../../images/svg"
+import { contact, list, svg } from "./Contact.module.scss"
+
+const Contact = () => {
+  return (
+    <section className={contact}>
+      <h4>Contact Us</h4>
+      <ul className={list}>
+        <li>
+          <div className={svg}>
+            <LocationPin />
+          </div>
+          <p>
+            Baskal's Garage, Triq Il-Provigarju,
+            <br />
+            Għajnsielem, Gozo
+          </p>
+        </li>
+        <li>
+          <div className={svg}>
+            <Clock />
+          </div>
+          <p>Mon - Sat, 7AM - 8PM</p>
+        </li>
+        <li>
+          <div className={svg}>
+            <Phone />
+          </div>
+          <p>(356) 2155 1154 / 7755 1154 </p>
+        </li>
+        <li>
+          <div className={svg}>
+            <Mail />
+          </div>
+          <p>baskal@baskalsgarage.com</p>
+        </li>
+        <li>
+          <div className={svg}>
+            <Facebook />
+          </div>
+          <p>fb.me/baskalsgarage</p>
+        </li>
+      </ul>
+    </section>
+  )
+}
+
+export default Contact
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,13 @@
 import React from "react"
+import Contact from "../Contact/Contact"
 import { footer } from "./Footer.module.scss"
 
 const Footer = () => {
-  return <footer className={footer}></footer>
+  return (
+    <footer className={footer}>
+      <Contact />
+    </footer>
+  )
 }
 
 export default Footer
