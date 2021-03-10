e5bde92c3221c89adc447043556e59d6446e1caf
Carlos <53526597+carlosgauci@users.noreply.github.com>
2021-03-10 18:22:30 +0100

    added intersectionObserver to google maps

--- src/components/FindUs/FindUs.js
+++ src/components/FindUs/FindUs.js
@@ -1,14 +1,18 @@
 import React from "react"
+import { useInView } from "react-intersection-observer"
 import GoogleMaps from "../GoogleMaps/GoogleMaps"
 import { findUs, map } from "./FindUs.module.scss"
 
 const FindUs = () => {
+  const { ref, inView } = useInView({
+    threshold: 0,
+    rootMargin: "300px",
+    triggerOnce: true,
+  })
   return (
-    <section className={findUs}>
+    <section className={findUs} ref={ref}>
       <h4>Get Directions</h4>
-      <div className={map}>
-        <GoogleMaps />
-      </div>
+      <div className={map}>{inView && <GoogleMaps />}</div>
     </section>
   )
 }
