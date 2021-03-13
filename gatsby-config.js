2cf9966ff11dc6df3394b3a5a5c85a80b930bf4c
Carlos <53526597+carlosgauci@users.noreply.github.com>
2021-03-13 21:27:15 +0100

    changed favicon and theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,10 +33,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#165bb5`,
+        theme_color: `#165bb5`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`,
       },
     },
     `gatsby-plugin-gatsby-cloud`,
