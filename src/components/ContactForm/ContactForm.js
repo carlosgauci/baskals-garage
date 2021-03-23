1dec6d20538528e24f7b8e38fdf7335a7b5bcfc2
Carlos <53526597+carlosgauci@users.noreply.github.com>
2021-03-23 10:39:58 +0100

    changed contact form not to redirect, but show success message instead

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,6 +1,6 @@
 // Contact form on the index page
 
-import React from "react"
+import React, { useState } from "react"
 import {
   container,
   form,
@@ -8,64 +8,113 @@ import {
   email,
   phone,
   company,
+  success,
 } from "./ContactForm.module.scss"
 
 const ContactForm = () => {
+  const [sent, setSent] = useState(false)
+  const [message, setMessage] = useState("")
+
+  // Contact form submit
+  const onSubmit = async e => {
+    e.preventDefault()
+    const formElements = [...e.currentTarget.elements]
+
+    // Check honeypot
+    const isValid =
+      formElements.filter(elem => elem.name === "bot-field")[0].value === ""
+    const validFormElements = isValid ? formElements : []
+    if (validFormElements.length < 1) {
+      setMessage("Looks like you've filled in too many fields..")
+    } else {
+      // Send form
+      const filledOutElements = validFormElements
+        .filter(elem => !!elem.value)
+        .map(
+          element =>
+            encodeURIComponent(element.name) +
+            "=" +
+            encodeURIComponent(element.value)
+        )
+        .join("&")
+
+      await fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: filledOutElements,
+      })
+        .then(() => {
+          setMessage(
+            "Your message was sent successfully, we'll contact you back as soon as possible!"
+          )
+          setSent(true)
+        })
+        .catch(_ => {
+          setMessage(
+            "There was an error submitting the form, please email us at baskal@baskalsgarage.com"
+          )
+        })
+    }
+  }
+
   return (
     <section className={container} id="contact">
-      <h2>Get a Quote</h2>
+      {!sent ? <h2>Get a Quote</h2> : <h2>Success!</h2>}
       {/* Netlify contact form, sends user to /success page after submit */}
-      <form
-        className={form}
-        name="contact-form"
-        method="post"
-        netlify-honeypot="bot-field"
-        data-netlify="true"
-        action="/success"
-      >
-        <input type="hidden" name="bot-field" />
-        <input type="hidden" name="form-name" value="contact-form" />
+      {!sent && (
+        <form
+          className={form}
+          name="contact-form"
+          method="post"
+          netlify-honeypot="bot-field"
+          data-netlify="true"
+          onSubmit={e => onSubmit(e)}
+        >
+          <input type="hidden" name="bot-field" />
+          <input type="hidden" name="form-name" value="contact-form" />
 
-        <input
-          type="text"
-          name="name"
-          placeholder="Your name"
-          aria-label="Your Name"
-          className={name}
-        />
+          <input
+            type="text"
+            name="name"
+            placeholder="Your name"
+            aria-label="Your Name"
+            className={name}
+          />
 
-        <input
-          type="email"
-          name="email"
-          placeholder="Your email address"
-          aria-label="Your email address"
-          className={email}
-        />
+          <input
+            type="email"
+            name="email"
+            placeholder="Your email address"
+            aria-label="Your email address"
+            className={email}
+          />
 
-        <input
-          type="number"
-          name="number"
-          placeholder="Phone number"
-          aria-label="Your Phone Number"
-          className={phone}
-        />
+          <input
+            type="number"
+            name="number"
+            placeholder="Phone number"
+            aria-label="Your Phone Number"
+            className={phone}
+          />
 
-        <input
-          type="text"
-          name="company"
-          placeholder="Company (optional)"
-          aria-label="Company (optional)"
-          className={company}
-        />
+          <input
+            type="text"
+            name="company"
+            placeholder="Company (optional)"
+            aria-label="Company (optional)"
+            className={company}
+          />
 
-        <textarea
-          name="message"
-          rows="7"
-          placeholder="Type your message here..."
-          aria-label="Your Message"
-        ></textarea>
-        <button type="submit">Submit</button>
-      </form>
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Type your message here..."
+            aria-label="Your Message"
+          ></textarea>
+          <button type="submit">Submit</button>
+        </form>
+      )}
+      {message && <p className={success}>{message}</p>}
     </section>
   )
 }
